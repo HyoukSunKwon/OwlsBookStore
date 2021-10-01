7949490ffff91fa9c731ef79b8ee427569a81b49
Skylar Kwon <ddauppo@gmail.com>
2021-10-01 11:09:10 -0400

    Install Autofac

---
+++ OwlsBookStore.Data/Services/IOwlsBookStoreData.cs
@@ -0,0 +1,14 @@
+﻿using OwlsBookStore.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwlsBookStore.Data.Services
+{
+    public interface IOwlsBookStoreData
+    {
+        IEnumerable<Writer> GetAll();
+    }
+}
---
+++ OwlsBookStore.Data/Services/OwlsBookStoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using OwlsBookStore.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwlsBookStore.Data.Services
+{
+    public class OwlsBookStoreDbContext : DbContext
+    {
+        public DbSet<Writer> writers { get; set; }
+    }
+}
