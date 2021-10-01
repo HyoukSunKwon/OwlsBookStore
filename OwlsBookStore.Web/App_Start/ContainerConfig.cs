2872c138a3469633d2b38b9c365e5e5854b5bd4d
Skylar Kwon <ddauppo@gmail.com>
2021-10-01 11:45:41 -0400

    Configuring the Autofac Inversion of controler container and configuring a DbContext

---
+++ OwlsBookStore.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using OwlsBookStore.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace OwlsBookStore.Web
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterCountainer(HttpConfiguration httpConfiguration)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<SqlOwlsBokStoreData>()
+                   .As<IOwlsBookStoreData>()
+                   .InstancePerRequest();
+            builder.RegisterType<OwlsBookStoreDbContext>().InstancePerRequest();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
